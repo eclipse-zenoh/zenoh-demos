bacfaf4d3e7d1029bc7833e102b3e71d73975219
Julien Enoch <julien.enoch@adlinktech.com>
2022-04-23 16:02:01 +0200

    Add --time-scale and --twist options (Twist deserialization+scale is now optional)

--- ROS2/zenoh-rust-replay/src/bin/ros2-replay.rs
+++ ROS2/zenoh-rust-replay/src/bin/ros2-replay.rs
@@ -12,22 +12,25 @@
 //   ADLINK zenoh team, <zenoh@adlink-labs.tech>
 //
 use async_std::task;
+use cdr::CdrLe;
 use cdr::Infinite;
 use clap::{App, Arg};
 use futures::prelude::*;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use zenoh::buf::reader::HasReader;
+use zenoh::buf::ZBuf;
 use zenoh::config::Config;
 use zenoh::query::*;
 use zenoh::queryable;
-#[derive(Deserialize, Debug, PartialEq)]
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 struct Vector3 {
     x: f64,
     y: f64,
     z: f64,
 }
 
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 struct Twist {
     linear: Vector3,
     angular: Vector3,
@@ -38,45 +41,57 @@ async fn main() {
     // Initiate logging
     env_logger::init();
 
-    let (config, cmd_vel, iscope, oscope, opath, filter, angular_scale, linear_scale) =
+    let (config, query_selector, pub_expr, time_scale, is_twist, angular_scale, linear_scale) =
         parse_args();
 
     println!("Opening session...");
     let session = zenoh::open(config).await.unwrap();
 
-    let mut input = iscope;
-    input.push_str(cmd_vel.as_str());
-    if !input.is_empty() {
-        input.push('?');
-        input.push_str(filter.as_str());
-    }
-
-    let output = if let Some(path) = opath {
-        path
-    } else {
-        let mut output = oscope;
-        output.push_str(cmd_vel.as_str());
-        output
-    };
-
-    println!("Sending Query '{}'...", input);
+    // Get stored publications
+    println!("Sending Query '{}'...", query_selector);
     let tgt = QueryTarget {
         kind: queryable::STORAGE,
         target: Target::All,
     };
     let mut replies = session
-        .get(&input)
+        .get(&query_selector)
         .target(tgt)
         .await
         .unwrap()
         .collect::<Vec<Reply>>()
         .await;
+    // Sort publications by timestamps
     replies.sort_by(|a, b| a.sample.timestamp.partial_cmp(&b.sample.timestamp).unwrap());
+
+    if replies.is_empty() {
+        println!("No publication found - nothing to replay.");
+        return;
+    }
+
+    let first_ts = replies.first().unwrap().sample.timestamp.unwrap();
+    let last_ts = replies.last().unwrap().sample.timestamp.unwrap();
+    println!(
+        "Replay {} publications that were made between {} and {} ",
+        replies.len(),
+        first_ts.get_time(),
+        last_ts.get_time(),
+    );
+    println!(
+        "Initial duration: {} seconds => with time-scale={}, new duration: {} seconds",
+        last_ts.get_diff_duration(&first_ts).as_secs_f32(),
+        time_scale,
+        last_ts
+            .get_diff_duration(&first_ts)
+            .mul_f64(time_scale)
+            .as_secs_f32(),
+    );
+
     let mut ts = None;
     for reply in replies {
+        // compute time difference and sleep (*time_scale)
         let now = match (ts, reply.sample.timestamp) {
             (Some(t1), Some(t2)) => {
-                task::sleep(t2.get_diff_duration(&t1)).await;
+                task::sleep(t2.get_diff_duration(&t1).mul_f64(time_scale)).await;
                 t2
             }
             (None, Some(t2)) => t2,
@@ -84,39 +99,41 @@ async fn main() {
         };
         ts = reply.sample.timestamp;
 
-        let mut cmd =
-            cdr::deserialize_from::<_, Twist, _>(reply.sample.value.payload.reader(), Infinite)
-                .unwrap();
-        cmd.linear.x *= linear_scale;
-        cmd.linear.y *= linear_scale;
-        cmd.linear.z *= linear_scale;
-        cmd.angular.x *= angular_scale;
-        cmd.angular.y *= angular_scale;
-        cmd.angular.z *= angular_scale;
         println!(
-            "[{}] Replay from '{}' to '{}':\n     '{:?}'",
+            "[{}] Replay publication from '{}' to '{}'",
             now.get_time(),
             reply.sample.key_expr,
-            output,
-            cmd
+            pub_expr
         );
-        session
-            .put(output.as_str(), reply.sample.value.payload)
-            .await
-            .unwrap();
+
+        if is_twist {
+            // payload is a Twist, apply scales and replay
+            let new_payload =
+                transform_twist(&reply.sample.value.payload, linear_scale, angular_scale);
+            session.put(pub_expr.as_str(), new_payload).await.unwrap();
+        } else {
+            // replay payload unchanged
+            session
+                .put(pub_expr.as_str(), reply.sample.value.payload)
+                .await
+                .unwrap();
+        }
     }
 }
 
-fn parse_args() -> (
-    Config,
-    String,
-    String,
-    String,
-    Option<String>,
-    String,
-    f64,
-    f64,
-) {
+fn transform_twist(payload: &ZBuf, linear_scale: f64, angular_scale: f64) -> Vec<u8> {
+    let mut twist = cdr::deserialize_from::<_, Twist, _>(payload.reader(), Infinite).unwrap();
+    twist.linear.x *= linear_scale;
+    twist.linear.y *= linear_scale;
+    twist.linear.z *= linear_scale;
+    twist.angular.x *= angular_scale;
+    twist.angular.y *= angular_scale;
+    twist.angular.z *= angular_scale;
+    println!("   '{:?}'", twist);
+    cdr::serialize::<_, _, CdrLe>(&twist, Infinite).unwrap()
+}
+
+fn parse_args() -> (Config, String, String, f64, bool, f64, f64) {
     let args = App::new("zenoh-net sub example")
         .arg(
             Arg::from_usage("-m, --mode=[MODE] 'The zenoh session mode (peer by default).")
@@ -129,29 +146,24 @@ fn parse_args() -> (
             "-c, --config=[FILE] 'A configuration file.'",
         ))
         .arg(
-            Arg::from_usage("--cmd_vel=[topic] 'The 'cmd_vel' ROS2 topic.'")
-                .default_value("/rt/turtle1/cmd_vel"),
-        )
-        .arg(
             Arg::from_usage("-f, --filter=[String] 'The 'filter' for querying. E.g. \"starttime=now()-1m;stoptime=now()\"'")
                 .default_value("(starttime=0)"),
         )
         .arg(
-            Arg::from_usage("-i, --input-scope=[String] 'A string added as prefix to all routed DDS topics when mapped to a zenoh resource.'")
-                .default_value(""),
+            Arg::from_usage("-i, --input-path=[String] 'A complete overwrite of the input zenoh resrouce (option -i will be ignored).'")
+                .default_value("/rt/turtle1/cmd_vel"),
         )
         .arg(
-            Arg::from_usage("-o, --output-scope=[String] 'A string added as prefix to all routed DDS topics when mapped to a zenoh resource.'")
-                .default_value(""),
+            Arg::from_usage("-o, --output-path=[String] 'A complete overwrite of the output zenoh resrouce (option -o will be ignored).'")
+                .default_value("/replay/rt/turtle1/cmd_vel"),
         )
+        .arg(Arg::from_usage("-t, --time-scale=[FLOAT] 'The time scale (i.e. multiplier of time interval between each re-publication).").default_value("1.0"))
+        .arg(Arg::from_usage("-w, --twist 'The data is a ROS2 Twist message. --angular-scale and --linear-scale will appli"))
         .arg(
-            Arg::from_usage("--output-path=[String] 'A complete overwrite of the output zenoh resrouce (option -o will be ignored).'"),
-        )
-        .arg(
-            Arg::from_usage("-a, --angular_scale=[FLOAT] 'The angular scale.'")
+            Arg::from_usage("-a, --angular-scale=[FLOAT] 'The angular scale (apply only if  --twist is set).'")
                 .default_value("1.0"),
         )
-        .arg(Arg::from_usage("-x, --linear_scale=[FLOAT] 'The linear scale.").default_value("1.0"))
+        .arg(Arg::from_usage("-x, --linear-scale=[FLOAT] 'The linear scale (apply only if  --twist is set).").default_value("1.0"))
         .get_matches();
 
     let mut config = if let Some(conf_file) = args.value_of("config") {
@@ -178,21 +190,28 @@ fn parse_args() -> (
         config.scouting.multicast.set_enabled(Some(false)).unwrap();
     }
 
-    let cmd_vel = args.value_of("cmd_vel").unwrap().to_string();
-    let iscope = args.value_of("input-scope").unwrap().to_string();
-    let oscope = args.value_of("output-scope").unwrap().to_string();
-    let opath = args.value_of("output-path").map(|s| s.to_string());
+    let ipath = args.value_of("input-path").unwrap().to_string();
+    let opath = args.value_of("output-path").unwrap().to_string();
     let filter = args.value_of("filter").unwrap().to_string();
-    let angular_scale: f64 = args.value_of("angular_scale").unwrap().parse().unwrap();
-    let linear_scale: f64 = args.value_of("linear_scale").unwrap().parse().unwrap();
+    let time_scale: f64 = args.value_of("time-scale").unwrap().parse().unwrap();
+    let is_twist = args.is_present("twist");
+    let angular_scale: f64 = args.value_of("angular-scale").unwrap().parse().unwrap();
+    let linear_scale: f64 = args.value_of("linear-scale").unwrap().parse().unwrap();
+
+    let mut query_selector = ipath;
+    if !filter.is_empty() {
+        query_selector.push('?');
+        query_selector.push_str(filter.as_str());
+    }
+
+    let pub_expr = opath;
 
     (
         config,
-        cmd_vel,
-        iscope,
-        oscope,
-        opath,
-        filter,
+        query_selector,
+        pub_expr,
+        time_scale,
+        is_twist,
         angular_scale,
         linear_scale,
     )
