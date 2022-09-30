7cd37961eeca0fa6aadafb0d92e92d0333976861
Julien Loudet <julien.loudet@zettascale.tech>
2022-09-30 15:10:39 +0200

    update `source-cmd-file` to Zenoh-Flow v0.4.0 (dev)

--- zenoh-flow-bot3/source-cmd-file/src/lib.rs
+++ zenoh-flow-bot3/source-cmd-file/src/lib.rs
@@ -12,90 +12,85 @@
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
 
-use async_trait::async_trait;
-
 use async_std::sync::Arc;
+use async_std::sync::Mutex;
+use async_trait::async_trait;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-
-use zenoh_flow::{
-    zenoh_flow_derive::ZFState, Configuration, Context, Data, Node, Source, State, ZFResult,
-};
+use zenoh_flow::prelude::*;
 use zenoh_flow_example_types::GamepadInput;
-
-#[derive(Debug, ZFState)]
-pub struct GamepadState {
-    reader: BufReader<File>,
-    input: GamepadInput,
-}
 
 pub struct GamepadSource;
 
-impl Node for GamepadSource {
-    fn initialize(&self, configuration: &Option<Configuration>) -> ZFResult<State> {
-
-        let file = match configuration {
-            Some(c) => {
-                File::open(c["file"].as_str().unwrap()).unwrap()
-            },
-            None => File::open("/tmp/commands-flow.txt")?,
-        };
-
-        Ok(State::from(GamepadState {
-            reader: BufReader::new(file),
-            input: GamepadInput::default(),
-        }))
-    }
-
-    fn finalize(&self, _: &mut State) -> ZFResult<()> {
-        Ok(())
-    }
-}
-
 #[async_trait]
 impl Source for GamepadSource {
-    async fn run(&self, _: &mut Context, state: &mut State) -> ZFResult<Data> {
-        let mut state = state.try_get::<GamepadState>()?;
+    async fn setup(
+        &self,
+        _context: &mut Context,
+        configuration: &Option<Configuration>,
+        mut outputs: Outputs,
+    ) -> Result<Option<Box<dyn AsyncIteration>>> {
+        let file = match configuration {
+            Some(c) => File::open(c["file"].as_str().unwrap()).unwrap(),
+            None => File::open("/tmp/commands-flow.txt")?,
+        };
+        let reader = Arc::new(Mutex::new(BufReader::new(file)));
+        let output = outputs
+            .take_into_arc("out")
+            .expect("Could not find output < out >");
 
-        // Read line and execute action
-        let mut line = String::new();
-        state.reader.read_line(&mut line)?;
-        match line.as_str() {
-            "left\n" => {
-                state.input.left_trigger = 0.0;
-                state.input.right_trigger = 0.0;
-                state.input.left_stick_x = -1.0;
-            }
-            "right\n" => {
-                state.input.left_trigger = 0.0;
-                state.input.right_trigger = 0.0;
-                state.input.left_stick_x = 1.0;
-            }
-            "forward\n" => {
-                state.input.left_trigger = 0.0;
-                state.input.right_trigger = 1.0;
-                state.input.left_stick_x = 0.0;
-            }
-            "backward\n" => {
-                state.input.left_trigger = 1.0;
-                state.input.right_trigger = 0.0;
-                state.input.left_stick_x = 0.0;
-            }
-            "stop\n" => {
-                state.input.left_trigger = 0.0;
-                state.input.right_trigger = 0.0;
-                state.input.left_stick_x = 0.0;
-            }
-            line => println!("I read {line} so I do know what to do..."),
-        }
+        Ok(Some(Box::new(move || {
+            let output = Arc::clone(&output);
+            let reader = Arc::clone(&reader);
 
-        Ok(Data::from(state.input))
+            async move {
+                let mut gamepad_input = GamepadInput::default();
+                let mut line = String::new();
+                {
+                    let mut reader = reader.lock().await;
+                    reader.read_line(&mut line)?;
+                }
+
+                match line.as_str() {
+                    "left\n" => {
+                        gamepad_input.left_trigger = 0.0;
+                        gamepad_input.right_trigger = 0.0;
+                        gamepad_input.left_stick_x = -1.0;
+                    }
+                    "right\n" => {
+                        gamepad_input.left_trigger = 0.0;
+                        gamepad_input.right_trigger = 0.0;
+                        gamepad_input.left_stick_x = 1.0;
+                    }
+                    "forward\n" => {
+                        gamepad_input.left_trigger = 0.0;
+                        gamepad_input.right_trigger = 1.0;
+                        gamepad_input.left_stick_x = 0.0;
+                    }
+                    "backward\n" => {
+                        gamepad_input.left_trigger = 1.0;
+                        gamepad_input.right_trigger = 0.0;
+                        gamepad_input.left_stick_x = 0.0;
+                    }
+                    "stop\n" => {
+                        gamepad_input.left_trigger = 0.0;
+                        gamepad_input.right_trigger = 0.0;
+                        gamepad_input.left_stick_x = 0.0;
+                    }
+                    line => println!("I read {line} so I do know what to do..."),
+                }
+
+                output.send_async(Data::from(gamepad_input), None).await?;
+
+                Ok(())
+            }
+        })))
     }
 }
 
 zenoh_flow::export_source!(register);
 
-fn register() -> ZFResult<Arc<dyn Source>> {
+fn register() -> Result<Arc<dyn Source>> {
     Ok(Arc::new(GamepadSource) as Arc<dyn Source>)
 }
