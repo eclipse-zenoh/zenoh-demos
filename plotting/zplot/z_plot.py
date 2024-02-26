d8c6e8191fc9ebeafe057a8415ef68f2264a3617
OlivierHecart <olivier.hecart@adlinktech.com>
2024-02-26 14:24:39 +0100

    Fix z_plot.py when timestamp is present

--- plotting/zplot/z_plot.py
+++ plotting/zplot/z_plot.py
@@ -55,7 +55,7 @@ def listener(sample):
         lines[str(sample.key_expr)] = ax.plot([], [], '-o', label=str(sample.key_expr))[0]
     now = time.time()
     xdata, ydata = lines[str(sample.key_expr)].get_data()
-    xdata = numpy.append(xdata, datetime.fromtimestamp(now if sample.timestamp is None else sample.timestamp.time))
+    xdata = numpy.append(xdata, datetime.fromtimestamp(now if sample.timestamp is None else sample.timestamp.time/4294967295))
     ydata = numpy.append(ydata, float(sample.payload.decode("utf-8")))
     lines[str(sample.key_expr)].set_data(zip(*filter(lambda t: t[0].timestamp() > now - args.history, zip(xdata, ydata))))
 
