35078dd3d4eefc383e94d9c5c5f72f3c941ca4df
kydos <kydos@protonmail.com>
2024-07-17 22:04:11 +0200

    Improved reconnection logic

--- zenoh-android/LocationTracker/app/src/main/java/tech/zettascale/demo/location/MainActivity.kt
+++ zenoh-android/LocationTracker/app/src/main/java/tech/zettascale/demo/location/MainActivity.kt
@@ -14,7 +14,6 @@
     import androidx.compose.material3.Text
     import androidx.compose.runtime.Composable
     import androidx.compose.ui.Modifier
-    import androidx.compose.ui.tooling.preview.Preview
     import androidx.core.app.ActivityCompat
     import androidx.core.content.ContextCompat
     import tech.zettascale.demo.location.ui.theme.LocationTrackerTheme
@@ -27,23 +26,9 @@
     import androidx.compose.foundation.layout.Arrangement
     import androidx.compose.foundation.layout.Box
     import androidx.compose.foundation.layout.Column
-    import androidx.compose.foundation.layout.PaddingValues
     import androidx.compose.foundation.layout.Row
-    import androidx.compose.foundation.layout.wrapContentSize
-    import androidx.compose.foundation.lazy.LazyColumn
-    import androidx.compose.material.icons.Icons
-    import androidx.compose.material.icons.filled.MoreVert
-    import androidx.compose.material.icons.outlined.Edit
-    import androidx.compose.material.icons.outlined.Email
-    import androidx.compose.material.icons.outlined.Settings
-    import androidx.compose.material3.HorizontalDivider
-    import androidx.compose.ui.text.style.TextAlign
     import androidx.compose.material3.Button
     import androidx.compose.material3.ButtonColors
-    import androidx.compose.material3.DropdownMenu
-    import androidx.compose.material3.DropdownMenuItem
-    import androidx.compose.material3.Icon
-    import androidx.compose.material3.IconButton
     import androidx.compose.material3.TextField
     import androidx.compose.runtime.getValue
     import androidx.compose.runtime.mutableStateOf
@@ -52,37 +37,136 @@
     import androidx.compose.ui.Alignment
     import androidx.compose.ui.graphics.Color
     import androidx.compose.ui.unit.dp
-
     import io.zenoh.Session
     import io.zenoh.Config
-    import io.zenoh.keyexpr.KeyExpr
     import io.zenoh.keyexpr.intoKeyExpr
     import io.zenoh.prelude.*
+    import io.zenoh.publication.Publisher
     import io.zenoh.value.Value
 
 
+    object LocatorState {
+        var lm: LocationManager? = null
+        private var locationListener: LocationListener? = null
+        private var z: Session? = null
+        private var zPub: Publisher? = null
+        private var activity: MainActivity? = null
+        private var running: Boolean = false
+        private var last_locator: String? = null
+        private var last_kind: String? = null
+        private const val UPDATE_PERIOD: Long = 1000
 
+        fun setupActivity(a: MainActivity) {
+            activity = a
+        }
+        private fun setupZenoh(locator: String, kind: String) {
+            if (z != null && locator == last_locator && kind == last_kind) {
+                Log.d("ZLoc", "Zenoh Runtime already setup for $locator and $kind")
+                return
+            } else {
+                last_locator = locator
+                last_kind = kind
+                // Close existing session and publisher
+                Log.d("ZLoc", "Cleaning up former zenoh Runtime")
+                if (zPub != null) {
+                    zPub!!.close()
+                    z!!.close()
+                }
+            }
+            Log.d("ZLoc", "Setting up zenoh Runtime")
+            val configJSON = """
+             {
+                 "mode": "client",
+                 "connect": {
+                     "endpoints": [
+                         "$locator"
+                     ]
+                 },
+                 "scouting": {
+                     "multicast": {
+                         "enabled": false
+                     }
+                 }
+             }
+            """.trimIndent()
+
+            val kexpr = "demo/tracker/mobs/$kind".intoKeyExpr().getOrThrow()
+            val c = Config.from(configJSON)
+
+            z = Session.open(c).getOrThrow()
+            zPub = z!!.declarePublisher(kexpr)
+                .congestionControl(CongestionControl.DROP)
+                .priority(Priority.REALTIME)
+                .res().getOrThrow()
+        }
+
+        @SuppressLint("MissingPermission")
+        fun startLocationTracking(locator: String,  model: String, color: String,  kind: String) {
+            running = true
+            Log.d("ZLoc", "Starting/Restarting Location Tracking")
+
+            setupZenoh(locator, kind)
+
+            Log.d("ZLoc", ">>>>>>>> Getting Location Manager")
+            lm = activity!!.getLocationManager()
+            val provider = lm!!.allProviders.find { s -> s == "fused" } ?: "gps"
+            Log.d("ZLoc", ">>>>>>>> Provider: $provider")
+
+            locationListener =
+                LocationListener { l ->
+                    val value = Value(locationToCarData(l, model, color, kind), Encoding(KnownEncoding.APP_JSON, ""))
+                    zPub!!.put(value).res()
+                    Log.d("ZLoc", ">>>>>>>> Published Location: $l")
+                }
+
+            val p: String = provider
+            val t: Long = UPDATE_PERIOD
+            val d = 0f
+
+            Log.d("ZLoc", ">>>>>>>> Registering Location Listener")
+            lm?.requestLocationUpdates(p, t, d, locationListener!!, Looper.getMainLooper())
+        }
+
+        fun stopLocationTracking() {
+            Log.d("ZLoc", "Stopping Location Tracking")
+            running = false
+            if (lm != null && locationListener != null) {
+                lm?.removeUpdates(locationListener!!)
+            }
+        }
+        private fun locationToCarData(l: Location, model: String, color: String, kind: String): String =
+            // TODO: get kind from UI
+            """{"position": {"lat": ${l.latitude},"lng": ${l.longitude}, "speed": ${l.speed*3.6},"color": "$color","id": "$model", "kind": "$kind"}"""
+
+
+    }
     class MainActivity : ComponentActivity() {
         companion object {
             private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         }
 
-        lateinit var lm: LocationManager
-        lateinit var provider: String
-        var locationListener: LocationListener? = null
-
+        fun getLocationManager(): LocationManager {
+            if (!isLocationPermissionGranted()) {
+                requestLocationPermission()
+            }
+            return getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+        override fun onDestroy() {
+            super.onDestroy()
+            Log.d("ZLoc", "Destroy Activity")
+        }
         @SuppressLint("MissingPermission")
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
+            LocatorState.setupActivity(this)
 
+            Log.d("ZLoc", "Starting Activity")
             enableEdgeToEdge()
             setContent {
                 LocationTrackerTheme {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                         Tracker(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding),
-                            this
+                            modifier = Modifier.padding(innerPadding)
                         )
                     }
                 }
@@ -103,75 +187,16 @@
                 LOCATION_PERMISSION_REQUEST_CODE)
         }
 
-        private fun locationToCarData(l: Location, model: String, color: String, kind: String): String =
-            // TODO: get kind from UI
-            """{"position": {"lat": ${l.latitude},"lng": ${l.longitude}}, "speed": ${l.speed*3.6},"color": "${color}","id": "${model}", "kind": "${kind}"}"""
 
-        @SuppressLint("MissingPermission")
-        public fun startLocationTracking(
-            locator: String,
-            model: String,
-            color: String,
-            kind: String, ) {
-            val config_json = """
-             {
-                 "mode": "client",
-                 "connect": {
-                     "endpoints": [
-                         "${locator}"
-                     ]
-                 },
-                 "scouting": {
-                     "multicast": {
-                         "enabled": false
-                     }
-                 }
-             }
-            """.trimIndent()
-
-            val kexpr = "demo/tracker/mobs/${kind}".intoKeyExpr().getOrThrow()
-            val c = Config.from(config_json)
-            val z = Session.open(c).getOrThrow()
-            val zpub = z.declarePublisher(kexpr)
-                .congestionControl(CongestionControl.DROP)
-                .priority(Priority.REALTIME)
-                .res().getOrThrow()
-
-            if (!isLocationPermissionGranted()) {
-                requestLocationPermission()
-            }
-
-            Log.d("ZLoc", ">>>>>>>> Creating Activity")
-            lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            provider = lm.allProviders.find { s -> s == "gps" } ?: "gps"
-
-            locationListener =
-                LocationListener { l ->
-                    var value = Value(locationToCarData(l, model, color, kind), Encoding(KnownEncoding.APP_JSON, ""))
-                    zpub.put(value).res()
-                    Log.d("ZLocTracker", ">>>>>>>> Published Location: $l")
-                }
-
-            val p: String = provider
-            val t: Long = 1000
-            val d: Float = 0f
-
-            lm.requestLocationUpdates(p, t, d, locationListener!!, Looper.getMainLooper())
-        }
-        fun stopLocationTracking() {
-            if (locationListener != null) {
-                lm.removeUpdates(locationListener!!)
-            }
-        }
 
     }
 
     @Composable
-    fun Tracker(name: String, modifier: Modifier = Modifier, activity: MainActivity) {
+    fun Tracker(modifier: Modifier = Modifier) {
         var locator by remember { mutableStateOf("tcp/3.71.106.121:7447") }
         var model by remember { mutableStateOf("Scrambler 1100") }
         var color by remember { mutableStateOf("Black") }
-        var running by remember {mutableStateOf(false)}
+        var running by remember {mutableStateOf(LocatorState.lm != null)}
         var kind by remember {mutableStateOf("motorbike")}
         Box(modifier = modifier,
             contentAlignment = Alignment.Center) {
@@ -223,7 +248,7 @@
                         colors = ButtonColors(Color.Green, Color.Black, Color.Gray, Color.Black),
                         onClick = {
                             running = true
-                            activity.startLocationTracking(locator, model, color, kind)
+                            LocatorState.startLocationTracking(locator, model, color, kind)
                         })
                     {
                         Text("Start")
@@ -232,7 +257,7 @@
                         colors = ButtonColors(Color.Red, Color.Black, Color.Gray, Color.Black),
                         onClick = {
                             running = false
-                            activity.stopLocationTracking()
+                            LocatorState.stopLocationTracking()
                         }
                     ) {
                         Text("Stop")
